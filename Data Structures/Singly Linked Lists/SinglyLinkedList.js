a86d90d88f400703d5eb290f0b497ad61e3c286a
Zubair <zubair.maqsood6@gmail.com>
2019-09-23 23:27:58 +0100

    more info

--- Data Structures/Singly Linked Lists/SinglyLinkedList.js
+++ Data Structures/Singly Linked Lists/SinglyLinkedList.js
@@ -75,17 +75,74 @@ class SinglyLinkedList{
 	// set the head property to be the current heads next property
 
 	shift(){
-		if (this.length === 0){
+		if (!this.head){
 			return undefined;
 		}
 
-		var current = this.head;
-		this.head = current.next;
+		var currentHead = this.head;
+		this.head = currentHead.next;
 		this.length--
+
+		if(this.length === 0){
+			this.tail = null;
+		}
+
+		return currentHead;
+
+	}
+
+
+	//this function should accept a value
+	// create a new node using the value passed to the function
+	// if there is no head property on the list, set the head property and tail to the newly created node
+	// otherwise set the newly created node's next property to the current head property on the list
+	// set the head property on the list to be that newly created node
+	// increment the length of the list by 1
+	// return linkedlist
+
+	unshift(val){
+		var newNode = new Node(val)
+		if (!this.head){
+			this.head = newNode
+			this.tail = this.head
+		} else {
+			var currentHead = this.head
+			newNode.next = currentHead
+
+			this.head = newNode;
+
+
+
+
+		}
+		this.length++
+		return this;
+
+	}
+
+	// function should accept an index
+	// if the index less than 0, greater than or equal to the length of list, return null
+	// loop through the list until you reach the index and return the node at the specific index
+
+	get(num){
+		if (typeof num !== "number"){
+			return undefined
+		}
+
+		if (num < 0 || num >= this.length) return null
+
+		var counter = 0;
+		var current = this.head;
+
+		while (counter !== num){
+			current = current.next
+			counter++;
+		}
 
 		return current;
 
 	}
+
 }
 
 
