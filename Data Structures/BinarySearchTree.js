f7115438c4e45c4000f3b064e5d6d8c375d4e738
Zubair <zubair.maqsood6@gmail.com>
2019-11-05 14:54:12 +0000

    find method on binary search tree

--- Data Structures/BinarySearchTree.js
+++ Data Structures/BinarySearchTree.js
@@ -28,6 +28,7 @@ class BinarySearchTree{
 		// if there is a root. check if the value of the new node is greater or less than the value of root
 			let current = this.root;
 			while(true){
+				if (value === current.value) return undefined;
 
 				// if it is less
 				if (value < current.value){
@@ -42,22 +43,63 @@ class BinarySearchTree{
 
 
 					}
-				} else if (value >)
+				// if it is greater
+				} else if (value > current.value){
+
+					//check to see if there is a node to the right
+					if (current.right === null){
+						// if there is not, add that node as the right property
+						current.right = newNode;
+						return this;
+					} else {
+						// if there is, move to that node and repeat the steps
+						current = current.right
+
+					}
+
+				}
 			}
 
 		}
 
+	}
 
-			// if it is greater
+
+	find(value){
+		// starting at the root
+		//check if there is a root, if not - we're done searching
+		if (this.root === null) return false;
+
+
+		// if there is a root, check if the value of new node is the value we are looking for
+		let current = this.root
+			found = false;
+
+			// if not, check to see if the value is greater or less than value of the root
+			while(current && !found){
+				// if it is less
+				//check to see if there is a node to the left
+				if (value < current.vale){
+					// if there is, move to that node and repeat these steps
+					current = current.left;
+
+				// if it is greater
 				//check to see if there is a node to the right
-				//if there is, move to that node and repeat these steps
-				// if there is not, add that node as the right property
+				} else if(value > current.value){
+					// if there is, move to that node and repeat these steps
+					current = current.right;
+				} else {
+					found = true;
+				}
+			}
+			if(!found) return false;
 
-
-
-				s
-
-
+			return current;
 
 	}
+
+
+
+
+
 }
